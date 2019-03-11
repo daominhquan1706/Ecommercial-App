aac12089867008b0a0d67aaef0ec4011f37b0df1
daominhquan176 <daominhquan176@gmail.com>
2019-03-11 19:41:54 +0700

    chuyển sang làm Mobile App

---
+++ Sourcecode Web/eCommercial/Controllers/HomeController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using eCommercial;
+
+namespace eCommercial.Controllers
+{
+    public class HomeController : Controller
+    {
+        private DefaultConnectionEntities db = new DefaultConnectionEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult DetailsProduct(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+        public ActionResult CategoryPick(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var listproduct = db.Products.Where(q=>q.ID_Category==id).Include(p => p.Category);
+            ViewBag.idCategory = id;
+            if (listproduct == null)
+            {
+                return HttpNotFound();
+            }
+            return View(listproduct.ToList());
+        }
+        [HttpPost]
+        public ActionResult Search(string keyword)
+        {
+            if (keyword == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var listproduct = db.Products.Where(q=>q.Product_Name.Contains(keyword)).Include(p => p.Category);
+            ViewBag.keyword = keyword;
+            if (listproduct == null)
+            {
+                return HttpNotFound();
+            }
+            return View(listproduct.ToList());
+        }
+        public ActionResult SavedProDuct()
+        {
+            if (!Request.IsAuthenticated)
+            {
+                return RedirectToAction("Login", "Account");
+            }
+            return View();
+        }
+    }
+}
---
+++ Sourcecode Web/eCommercial/signalr/hubs/eCommercialHubs.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using eCommercial;
+
+namespace eCommercial.signalr.hubs
+{
+    public class eCommercialHubs : Hub
+    {
+        DefaultConnectionEntities db = new DefaultConnectionEntities();
+        //view truyền vào
+        public void Comment(string content,int idproduct,string idcustomer)
+        {
+            Comment_Product cmt = new Comment_Product();
+            cmt.Comment_Content = content;
+            cmt.ID_Product = idproduct;
+            cmt.CreationTime = DateTime.Now;
+            cmt.IsDeleted = false;
+            cmt.ID_Customer = idcustomer;
+            db.Comment_Product.Add(cmt);
+            db.SaveChanges();
+            int count = db.Products.Find(idproduct).Comment_Product.Count();
+            string ngay = String.Format("{0:G}", DateTime.Now);
+            //hàm truyền ra view
+            Clients.All.addNewMessageToPage(content,db.Customers.Find(idcustomer).Avatar,db.Customers.Find(idcustomer).Last_Name, db.Customers.Find(idcustomer).First_Name, ngay,count);
+        }
+        public void Like(int idproduct,string idcustomer)
+        {
+            List<Product_Like> dblike = db.Product_Like.Where(q => q.ID_Customer == idcustomer && q.ID_Product == idproduct).ToList();
+            string toast = "";
+            bool isSave = true;
+            if (dblike.Count() ==0)
+            {
+                Product_Like like = new Product_Like();
+                like.ID_Product = idproduct;
+                like.ID_Customer = idcustomer;
+                like.CreationTime = DateTime.Now;
+                db.Product_Like.Add(like);
+                toast = "Product " + db.Products.Find(idproduct).Product_Name + " successfully saved";
+                isSave = true;
+            }
+            else
+            {
+                db.Product_Like.Remove(db.Product_Like.Find(dblike[0].Id));
+                toast = "Product " + db.Products.Find(idproduct).Product_Name + " successfully deleted";
+                isSave = false;
+            }
+            db.SaveChanges();
+
+            string countSavedProduct = db.Product_Like.Where(r => r.ID_Customer == idcustomer).Count().ToString();
+            
+            Clients.All.ChangeLikeButton(countSavedProduct,toast,idproduct,isSave);
+
+        }
+    }
+}
