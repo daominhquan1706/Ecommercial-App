6de5e3cb146bfd60c0c65bbdfe681b667ee11d55
daominhquan176 <daominhquan176@gmail.com>
2019-03-23 14:44:14 +0700

    update icon app

---
+++ SourceCode Android Application/Test1706/app/src/main/java/com/example/test1706/SplashScreenActivity.java
@@ -0,0 +1,16 @@
+package com.example.test1706;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
