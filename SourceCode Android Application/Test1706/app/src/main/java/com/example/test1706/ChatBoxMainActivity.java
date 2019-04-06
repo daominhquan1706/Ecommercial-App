f970455234d857dc7e21ad9c033aac2255447919
Hung <hung.lq.15dh110353@outlook.com>
2019-04-06 13:40:56 +0700

    update chatbos

--- SourceCode Android Application/Test1706/app/src/main/java/com/example/test1706/ChatBoxMainActivity.java
+++ SourceCode Android Application/Test1706/app/src/main/java/com/example/test1706/ChatBoxMainActivity.java
@@ -7,6 +7,8 @@ import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.text.format.DateFormat;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -14,33 +16,77 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
-import android.text.format.DateFormat;
+import android.widget.Toast;
 
+import com.example.test1706.model.Product;
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.database.FirebaseListAdapter;
 import com.firebase.ui.database.FirebaseListOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
 
 
 public class ChatBoxMainActivity extends AppCompatActivity {
-
-    private static int SIGN_IN_REQUEST_CODE=1;
-    private FirebaseListAdapter<ChatMessage> adapter;
+    List<ChatMessage> chatMessageList;
+    private static int SIGN_IN_REQUEST_CODE = 1;
     RelativeLayout activity_chatboxmain;
-    FloatingActionButton fab;
+    FloatingActionButton btn_send_message;
+    FirebaseDatabase database;
+    DatabaseReference myRef;
+    EditText input;
+    ListView listOfMessage;
+    private static final String TAG = "ChatBoxMainActivity";
+    CustomListAdapter customListAdapter;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chatboxmainactivity);
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference();
+        activity_chatboxmain = (RelativeLayout) findViewById(R.id.activity_chatboxmainactivity);
+        btn_send_message = (FloatingActionButton) findViewById(R.id.fab);
+        input = (EditText) findViewById(R.id.input);
+        if (FirebaseAuth.getInstance().getCurrentUser() == null) {
+            startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder().build(), SIGN_IN_REQUEST_CODE);
+        } else {
+            displayChatMessage();
+        }
+        btn_send_message.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Date date = new Date();
+                FirebaseDatabase.getInstance().getReference().child("chat_message").push().setValue(new ChatMessage(
+                        input.getText().toString(),
+                        FirebaseAuth.getInstance().getCurrentUser().getEmail(),
+                        System.currentTimeMillis(),
+                        "admin"));
+                input.setText("");
+                customListAdapter.notifyDataSetChanged();
+                scrollMyListViewToBottom();
+            }
+        });
+    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if(item.getItemId()==R.id.menu_sign_out)
-        {
+        if (item.getItemId() == R.id.menu_sign_out) {
             AuthUI.getInstance().signOut(this).addOnCompleteListener(new OnCompleteListener<Void>() {
                 @Override
                 public void onComplete(@NonNull Task<Void> task) {
-                    Snackbar.make(activity_chatboxmain,"You have been signed out",Snackbar.LENGTH_SHORT).show();
+                    Snackbar.make(activity_chatboxmain, "You have been signed out", Snackbar.LENGTH_SHORT).show();
                     finish();
 
                 }
@@ -51,86 +97,74 @@ public class ChatBoxMainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main_menu_chatbox,menu);
+        getMenuInflater().inflate(R.menu.main_menu_chatbox, menu);
         return true;
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode==SIGN_IN_REQUEST_CODE)
-        {
-            if(resultCode==RESULT_OK)
-            {
-                Snackbar.make(activity_chatboxmain,"Succesfully sign in",Snackbar.LENGTH_SHORT).show();
+        if (requestCode == SIGN_IN_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                Snackbar.make(activity_chatboxmain, "Succesfully sign in", Snackbar.LENGTH_SHORT).show();
                 displayChatMessage();
-            }
-            else
-            {
-                Snackbar.make(activity_chatboxmain,"TRY AGAIN !!!!",Snackbar.LENGTH_SHORT).show();
+            } else {
+                Snackbar.make(activity_chatboxmain, "TRY AGAIN !!!!", Snackbar.LENGTH_SHORT).show();
                 finish();
             }
         }
     }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_chatboxmainactivity);
 
-        activity_chatboxmain= (RelativeLayout) findViewById(R.id.activity_chatboxmainactivity);
-        fab= (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+    private void displayChatMessage() {
+        listOfMessage = (ListView) findViewById(R.id.list_of_message);
+        chatMessageList = new ArrayList<ChatMessage>();
+        customListAdapter = new CustomListAdapter(this, chatMessageList);
+        listOfMessage.setAdapter(customListAdapter);
+
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference();
+        myRef.child("chat_message").addChildEventListener(new ChildEventListener() {
             @Override
-            public void onClick(View v) {
-                EditText input= (EditText) findViewById(R.id.input);
-                FirebaseDatabase.getInstance().getReference().push().setValue(new ChatMessage(input.getText().toString(),
-                        FirebaseAuth.getInstance().getCurrentUser().getEmail()));
-                input.setText("");
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                ChatMessage itemProduct = dataSnapshot.getValue(ChatMessage.class);
+                chatMessageList.add(itemProduct);
+                Log.d(TAG, "onChildAdded: " + itemProduct.getMessageUser());
+                customListAdapter.notifyDataSetChanged();
+                scrollMyListViewToBottom();
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
             }
         });
 
 
-        if(FirebaseAuth.getInstance().getCurrentUser()==null)
-        {
-            startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder().build(),SIGN_IN_REQUEST_CODE);
-
-        }
-        else {
-            Snackbar.make(activity_chatboxmain,"Welcome"+FirebaseAuth.getInstance().getCurrentUser().getEmail(),Snackbar.LENGTH_SHORT).show();
-
-            displayChatMessage();
-        }
-
     }
-
-    private void displayChatMessage() {
-
-        ListView listOfMessage= (ListView) findViewById(R.id.list_of_message);
-
-        FirebaseListOptions<ChatMessage> options = new FirebaseListOptions.Builder<ChatMessage>()
-                .build();
-
-
-
-        adapter=new FirebaseListAdapter<ChatMessage>(options)
-            {
-
-
-                @Override
-                protected void populateView(View v, ChatMessage model, int position) {
-                    TextView messageText,messageUser,messageTime;
-                    messageText= (TextView) v.findViewById(R.id.message_text);
-                    messageUser= (TextView) v.findViewById(R.id.message_user);
-                    messageTime= (TextView) v.findViewById(R.id.message_time);
-
-                    messageText.setText(model.getMessageText());
-                    messageUser.setText(model.getMessageUser());
-                    messageTime.setText(DateFormat.format("dd-MM-yy (HH:mm:ss)",model.getMessageTime()));
-                }
-            };
-        listOfMessage.setAdapter(adapter);
-
-        }
+    private void scrollMyListViewToBottom() {
+        listOfMessage.post(new Runnable() {
+            @Override
+            public void run() {
+                // Select the last row so it will scroll into view...
+                listOfMessage.setSelection(customListAdapter.getCount() - 1);
+            }
+        });
     }
+}
 
--- SourceCode Android Application/Test1706/app/src/main/java/com/example/test1706/ChatMessage.java
+++ SourceCode Android Application/Test1706/app/src/main/java/com/example/test1706/ChatMessage.java
@@ -3,21 +3,29 @@ package com.example.test1706;
 import java.util.Date;
 
 public class ChatMessage {
-
+    private String messageUser_NguoiNhan;
     private String messageText;
     private String messageUser;
     private long messageTime;
+    public ChatMessage() {
+    }
 
 
-    public ChatMessage(String messageText, String messageUser) {
+    public ChatMessage(String messageText, String messageUser, long messageTime,String messageUser_NguoiNhan) {
+        this.messageUser_NguoiNhan = messageUser_NguoiNhan;
         this.messageText = messageText;
         this.messageUser = messageUser;
-
-        messageTime= new Date().getTime();
+        this.messageTime = messageTime;
     }
-    public ChatMessage(){
 
+    public String getMessageUser_NguoiNhan() {
+        return messageUser_NguoiNhan;
     }
+
+    public void setMessageUser_NguoiNhan(String messageUser_NguoiNhan) {
+        this.messageUser_NguoiNhan = messageUser_NguoiNhan;
+    }
+
 
     public String getMessageText() {
         return messageText;
