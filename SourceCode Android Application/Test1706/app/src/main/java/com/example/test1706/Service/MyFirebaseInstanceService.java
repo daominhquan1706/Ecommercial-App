07c55f287e01af0ac79c6c375d584ef9b7171699
Hung <hung.lq.15dh110353@outlook.com>
2019-04-22 19:36:42 +0700

    notification

---
+++ SourceCode Android Application/Test1706/app/src/main/java/com/example/test1706/Service/MyFirebaseInstanceService.java
@@ -0,0 +1,83 @@
+package com.example.test1706.Service;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+
+import com.example.test1706.R;
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import java.util.Map;
+import java.util.Random;
+
+public class MyFirebaseInstanceService extends FirebaseMessagingService {
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+
+        if(remoteMessage.getData().isEmpty())
+            showNotification(remoteMessage.getNotification().getTitle(),remoteMessage.getNotification().getBody());
+        else
+            showNotification(remoteMessage.getData());
+    }
+
+    private void showNotification(Map<String, String> data) {
+        String title=data.get("title").toString();
+        String body=data.get("body").toString();
+
+        NotificationManager notificationManager=(NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+        String NOTIFICATION_CHANNEL_ID="com.example.notification.test";
+
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.O){
+            NotificationChannel notificationChannel=new NotificationChannel(NOTIFICATION_CHANNEL_ID,"Notification",
+                    NotificationManager.IMPORTANCE_DEFAULT);
+            notificationChannel.setDescription("day la thong bao");
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(android.R.color.holo_blue_bright);
+            notificationChannel.setVibrationPattern(new long[]{0,1000,500,1000});
+            notificationChannel.enableLights(true);
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
+        NotificationCompat.Builder notificationbuilder =new NotificationCompat.Builder(this,NOTIFICATION_CHANNEL_ID);
+        notificationbuilder.setAutoCancel(true).setDefaults(Notification.DEFAULT_ALL).setWhen(System.currentTimeMillis()).setSmallIcon(R.drawable.ic_launcher_foreground).setContentTitle(title)
+                .setContentText(body).setContentInfo("Info");
+        notificationManager.notify(new Random().nextInt(),notificationbuilder.build());
+    }
+
+    private void showNotification(String title, String body) {
+        NotificationManager notificationManager=(NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+        String NOTIFICATION_CHANNEL_ID="com.example.notification.test";
+
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.O){
+            NotificationChannel notificationChannel=new NotificationChannel(NOTIFICATION_CHANNEL_ID,"Notification",
+                    NotificationManager.IMPORTANCE_DEFAULT);
+            notificationChannel.setDescription("day la thong bao");
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(android.R.color.holo_blue_bright);
+            notificationChannel.setVibrationPattern(new long[]{0,1000,500,1000});
+            notificationChannel.enableLights(true);
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
+        NotificationCompat.Builder notificationbuilder =new NotificationCompat.Builder(this,NOTIFICATION_CHANNEL_ID);
+        notificationbuilder.setAutoCancel(true).setDefaults(Notification.DEFAULT_ALL).setWhen(System.currentTimeMillis()).setSmallIcon(R.drawable.ic_launcher_foreground).setContentTitle(title)
+                .setContentText(body).setContentInfo("Info");
+        notificationManager.notify(new Random().nextInt(),notificationbuilder.build());
+
+    }
+
+    @Override
+    public void onNewToken(String s) {
+        super.onNewToken(s);
+
+        Log.d("TOKENFIREBASE",s);
+    }
+}
+
+
