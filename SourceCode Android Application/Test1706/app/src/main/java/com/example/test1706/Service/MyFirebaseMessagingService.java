110a28947c0dc0ae0ea2a566a1678c99ea338567
Hung <hung.lq.15dh110353@outlook.com>
2019-04-25 08:27:08 +0700

    professionally pop up

--- SourceCode Android Application/Test1706/app/src/main/java/com/example/test1706/Service/MyFirebaseMessagingService.java
+++ SourceCode Android Application/Test1706/app/src/main/java/com/example/test1706/Service/MyFirebaseMessagingService.java
@@ -1,38 +1,51 @@
 package com.example.test1706.Service;
 
-import android.app.Service;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
-import android.os.IBinder;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
+import com.example.test1706.MainActivity;
+import com.example.test1706.R;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
-public class MyFirebaseMessagingService extends FirebaseMessagingService {
-    private static final String TAG = "MyFirebaseMessagingServ";
-    public MyFirebaseMessagingService() {
-    }
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    private static final String TAG = "FirebaseMessageService";
+    Bitmap bitmap;
+
+    /**
+     * Called when message is received.
+     *
+     * @param remoteMessage Object representing the message received from Firebase Cloud Messaging.
+     */
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
-        // ...
-
-        // TODO(developer): Handle FCM messages here.
-        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
+        // There are two types of messages data messages and notification messages. Data messages are handled
+        // here in onMessageReceived whether the app is in the foreground or background. Data messages are the type
+        // traditionally used with GCM. Notification messages are only received here in onMessageReceived when the app
+        // is in the foreground. When the app is in the background an automatically generated notification is displayed.
+        // When the user taps on the notification they are returned to the app. Messages containing both notification
+        // and data payloads are treated as notification messages. The Firebase console always sends notification
+        // messages. For more see: https://firebase.google.com/docs/cloud-messaging/concept-options
+        //
         Log.d(TAG, "From: " + remoteMessage.getFrom());
 
         // Check if message contains a data payload.
         if (remoteMessage.getData().size() > 0) {
             Log.d(TAG, "Message data payload: " + remoteMessage.getData());
-
-            if (/* Check if data needs to be processed by long running job */ true) {
-                // For long-running tasks (10 seconds or more) use Firebase Job Dispatcher.
-                scheduleJob();
-            } else {
-                // Handle message within 10 seconds
-                handleNow();
-            }
-
         }
 
         // Check if message contains a notification payload.
@@ -40,33 +53,71 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
             Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
         }
 
-        // Also if you intend on generating your own notifications as a result of a received FCM
-        // message, here is where that should be initiated. See sendNotification method below.
+        //The message which i send will have keys named [message, image, AnotherActivity] and corresponding values.
+        //You can change as per the requirement.
+
+        //message will contain the Push Message
+        String message = remoteMessage.getData().get("message");
+        //imageUri will contain URL of the image to be displayed with Notification
+        String imageUri = remoteMessage.getData().get("image");
+        //If the key AnotherActivity has  value as True then when the user taps on notification, in the app AnotherActivity will be opened.
+        //If the key AnotherActivity has  value as False then when the user taps on notification, in the app MainActivity will be opened.
+        String TrueOrFlase = remoteMessage.getData().get("MainActivity");
+
+        //To get a Bitmap image from the URL received
+        bitmap = getBitmapfromUrl(imageUri);
+
+        sendNotification(message, bitmap, TrueOrFlase);
+
     }
+
+
     /**
-     * Called if InstanceID token is updated. This may occur if the security of
-     * the previous token had been compromised. Note that this is called when the InstanceID token
-     * is initially generated so this is where you would retrieve the token.
+     * Create and show a simple notification containing the received FCM message.
      */
-    @Override
-    public void onNewToken(String token) {
-        Log.d(TAG, "Refreshed token: " + token);
 
-        // If you want to send messages to this application instance or
-        // manage this apps subscriptions on the server side, send the
-        // Instance ID token to your app server.
-        sendRegistrationToServer(token);
+    private void sendNotification(String messageBody, Bitmap image, String TrueOrFalse) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        intent.putExtra("MainActivity", TrueOrFalse);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                .setLargeIcon(image)/*Notification icon image*/
+                .setSmallIcon(R.drawable.mapbox_logo_icon)
+                .setContentTitle(messageBody)
+                .setStyle(new NotificationCompat.BigPictureStyle()
+                        .bigPicture(image))/*Notification with Image*/
+                .setAutoCancel(true)
+                .setSound(defaultSoundUri)
+                .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
     }
 
-    private void sendRegistrationToServer(String token) {
+    /*
+     *To get a Bitmap image from the URL received
+     * */
+    public Bitmap getBitmapfromUrl(String imageUrl) {
+        try {
+            URL url = new URL(imageUrl);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setDoInput(true);
+            connection.connect();
+            InputStream input = connection.getInputStream();
+            Bitmap bitmap = BitmapFactory.decodeStream(input);
+            return bitmap;
 
-    }
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            return null;
 
-    private void handleNow() {
-
-    }
-
-    private void scheduleJob() {
-
+        }
     }
 }
