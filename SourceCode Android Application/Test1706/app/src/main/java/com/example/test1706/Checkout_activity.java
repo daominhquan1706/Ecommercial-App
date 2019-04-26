4a2d6da58f8c7d9577e4ae917b4cfdd0e811cad2
Hung <hung.lq.15dh110353@outlook.com>
2019-04-26 19:42:02 +0700

    admin + mua hàng tutorial

--- SourceCode Android Application/Test1706/app/src/main/java/com/example/test1706/Checkout_activity.java
+++ SourceCode Android Application/Test1706/app/src/main/java/com/example/test1706/Checkout_activity.java
@@ -16,6 +16,8 @@ import com.example.test1706.Config.Config;
 import com.example.test1706.model.Cart;
 import com.example.test1706.model.CartSqliteHelper;
 import com.example.test1706.model.Orders;
+import com.getkeepsafe.taptargetview.TapTarget;
+import com.getkeepsafe.taptargetview.TapTargetView;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
@@ -84,6 +86,11 @@ public class Checkout_activity extends AppCompatActivity {
 
         listView_adapter_checkout_item = new ListView_Adapter_checkout_item(this, cartSqliteHelper.getAllCarts());
         lv_checkout.setAdapter(listView_adapter_checkout_item);
+
+        TapTargetView.showFor(this,
+                TapTarget.forView(findViewById(R.id.btnPaynow), "Hướng dẫn sử dụng", "Click để dùng tài khoản PayPal thanh toán")
+                        .tintTarget(false)
+                        .outerCircleColor(R.color.MoneyColor));
     }
 
     public void init() {
